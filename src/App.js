92c015f26f33caec1498f25a5962e95bd78f671a
Satyam <bajpai1973satyam@gmail.com>
2023-01-07 23:30:05 +0530

    added distracted app screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Hello from "./component/Hello";
 import Main from "./component/Main";
 // import chrome from "chrome";
-// import Hello from "./components/Hello";
 function App() {
   return (
     <div className="App">
--- src/component/Main.js
+++ src/component/Main.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Timer from "../common/Timer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCoffee,
+  faGear,
+  faChartSimple,
+} from "@fortawesome/free-solid-svg-icons";
+import DisabledApps from "./DisabledApps";
+
 function Main() {
+  const [isDistractApps, setIsDistractApps] = useState(false);
   return (
-    <div>
-      <Timer />
+    <div className="main">
+      <div className="appbar">
+        <FontAwesomeIcon icon={faGear} size="2x" />
+        <div
+          onClick={() => setIsDistractApps(!isDistractApps)}
+          className="link"
+        >
+          {isDistractApps ? "Timer" : "Disabled Sites"}
+        </div>
+        <FontAwesomeIcon icon={faChartSimple} size="2x" />
+      </div>
+      {isDistractApps ? <DisabledApps /> : <Timer />}
     </div>
   );
 }
