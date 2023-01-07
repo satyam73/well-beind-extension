4b92d8e1e3ac174150322fcd0fc561aecb6583a6
Satyam <bajpai1973satyam@gmail.com>
2023-01-07 23:50:16 +0530

    done some basic style enhancements

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,27 +1,16 @@
 import React, { useState } from "react";
 import Timer from "../common/Timer";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCoffee,
-  faGear,
-  faChartSimple,
-} from "@fortawesome/free-solid-svg-icons";
 import DisabledApps from "./DisabledApps";
+import AppBar from "../common/AppBar";
 
 function Main() {
   const [isDistractApps, setIsDistractApps] = useState(false);
   return (
     <div className="main">
-      <div className="appbar">
-        <FontAwesomeIcon icon={faGear} size="2x" />
-        <div
-          onClick={() => setIsDistractApps(!isDistractApps)}
-          className="link"
-        >
-          {isDistractApps ? "Timer" : "Disabled Sites"}
-        </div>
-        <FontAwesomeIcon icon={faChartSimple} size="2x" />
-      </div>
+      <AppBar
+        isDistractApps={isDistractApps}
+        setIsDistractApps={setIsDistractApps}
+      />
       {isDistractApps ? <DisabledApps /> : <Timer />}
     </div>
   );
--- src/component/WebsiteRow.js
+++ src/component/WebsiteRow.js
@@ -1,4 +1,7 @@
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTrash,
+  faArrowUpRightFromSquare,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
@@ -6,7 +9,7 @@ function WebsiteRow() {
   return (
     <div className="website__row">
       <p className="website__rowText">Website row text</p>
-      <FontAwesomeIcon className="deleteIcon" icon={faTrash} size="2x" />
+      <FontAwesomeIcon className="icon deleteIcon" icon={faTrash} />
     </div>
   );
 }
